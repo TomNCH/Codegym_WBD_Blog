45492b4ec95e40728700ee9d503780a525a5e3e2
HieuNC <nguyenchihieu.10390@gamil.com>
2019-10-10 11:39:32 +0700

    fix error

---
+++ src/main/java/com/managers/config/ApplicationInitializer.java
@@ -0,0 +1,21 @@
+package com.managers.config;
+
+import com.managers.security.WebSecurityConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebSecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ApplicationConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
